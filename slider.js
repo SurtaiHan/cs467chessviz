a2a5deb9203e02a354c71917b6c523c008682dd4
Charles <cshao4@illinois.edu>
2016-04-25 20:45:56 -0500

    ok

--- slider.js
+++ slider.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
-    var slider = d3.slider().min(0).max(50).ticks(25).showRange(true).value(0);
+    var value  = 0;
+    var slider = d3.slider();
+    slider.min(0).max(50).ticks(25).showRange(true).value(0);
+    slider.callback(function() {
+        var next = Math.round(slider.value());
+        if (next == value) {
+            return;
+        }
+        value = next;
+        console.log(value);
+    });
     d3.select('#slider').call(slider);
 });
